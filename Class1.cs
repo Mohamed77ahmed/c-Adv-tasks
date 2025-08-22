8951649c682069e357ff128c01814b5c6bd3aaf9
mohamed ahmed <149879614+Mohamed77ahmed@users.noreply.github.com>
2025-08-22 17:15:22 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4
+{
+    public class Book
+    {
+
+
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Author { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+
+        public Book(string iSBN, string title, string[] author, DateTime publicationDate, decimal price)
+        {
+            ISBN = iSBN;
+            Title = title;
+            Author = author;
+            PublicationDate = publicationDate;
+            Price = price;
+        }
+
+
+        public override string ToString()
+        {
+            return $"ISBN {ISBN} ,Title {Title} , Author {Author} , PublicationDate {PublicationDate} , Price {Price}";
+        }
+
+    }
+
+
+    public class BookFunctions
+    {
+
+        public static string GetTitle(Book B)
+        {
+            return B.Title;
+        }
+
+        public static string GetAuthors(Book B)
+        {
+            return string.Join(", ", B.Author);
+        }
+
+        public static string GetPrice(Book B)
+        {
+            return B.Price.ToString("C");
+        }
+
+
+
+
+    }
+
+
+
+    public delegate string BookDelegate(Book b);
+
+
+
+    public class LibraryEngine
+    {
+        public static void ProcessBooks(List<Book> bList, BookDelegate fPtr)
+        {
+            foreach (Book b in bList)
+            {
+                Console.WriteLine(fPtr(b));
+            }
+        }
+
+
+        public static void ProcessBooks(List<Book> bList, Func<Book, string> fPtr)
+        {
+            foreach (Book b in bList)
+            {
+                Console.WriteLine(fPtr(b));
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Reflection.Metadata.BlobBuilder;
+
+namespace assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<Book> books = new List<Book>()
+        {
+            new Book("111", "C#", new string[]{"nada", "Sama"}, new DateTime(2025,5,1), 300),
+            new Book("222", "Advanced C#", new string[]{"Mohammed"}, new DateTime(2011,3,15), 500m)
+        };
+
+
+
+            LibraryEngine.ProcessBooks(books, new BookDelegate(BookFunctions.GetTitle));
+
+
+            Console.WriteLine("---------------------------------------------");
+
+            LibraryEngine.ProcessBooks(books, new BookDelegate(BookFunctions.GetAuthors));
+
+
+
+        }
+    }
+}
